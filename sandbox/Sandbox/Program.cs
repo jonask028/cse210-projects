f499e7958929fc7d2f05e5a98844db37e5c69450
jonask028 <jonas.kennedy.028@gmail.com>
2026-09-23 10:10:34 -0600

    in class sandbox work

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour tout le monde!");
+        // Console.WriteLine("Bonjour tout le monde!");
+
+        // bool done = false;
+
+        // while (!done)
+        // {
+        //     Console.Write("Are we done? (y/N)");
+        //     string input = Console.ReadLine();
+        //     if (input.Equals("y"))
+        //         done = true;
+        //     else
+        //         continue;
+
+        // }
+
+        // bool done;
+
+        // do
+        // {
+        //     Console.Write("Are we done? (y/N)");
+        //     string input = Console.ReadLine();
+        //     if (input.ToLower().Equals("y"))
+        //         done = true;
+        //     else
+        //         done = false;
+        // } while (!done);
+
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        List<string> itemList = ["Betty", "And", "The", "Jets"];
+        itemList.Prepend("Buh");
+
+        foreach (string item in itemList)
+            Console.WriteLine(item);
+
     }
 }
