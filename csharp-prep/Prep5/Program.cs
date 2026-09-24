2834f7c66d290815dbb77f02dde14996548a86d3
jonask028 <jonas.kennedy.028@gmail.com>
2026-09-24 12:32:59 -0600

    prep5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,71 @@ using System;
 
 class Program
 {
+
+    static void SayHello()
+    {
+        Console.WriteLine("Hello! I have some questions for you!");
+    }
+
+    static string GetName()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int GetFavNum()
+    {
+        Console.Write("What is your favorite number? ");
+        string faveNumString = Console.ReadLine();
+        try
+        {
+            int faveNum = int.Parse(faveNumString);
+            return faveNum;
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Error: {faveNumString} is not a number!");
+            throw new FormatException($"Error Message: {ex.Message}");
+        }
+    }
+
+    static int GetBirthYear(out int birthYear)
+    {
+        Console.Write("What is your birth year? ");
+        string birthYearString = Console.ReadLine();
+        try
+        {
+            birthYear = int.Parse(birthYearString);
+            return birthYear;
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Error: {birthYearString} is not a number!");
+            throw new FormatException($"Error Message: {ex.Message}");
+        }
+    }
+
+    static int SquareNum(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, int squaredNum, int birthYear)
+    {
+        int age = DateTime.Now.Year - birthYear;
+        Console.WriteLine($"{name} the square of your favorite number is {squaredNum}");
+        Console.WriteLine($"{name}, you will turn {age} this year!");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int birthYear;
+        SayHello();
+        string name = GetName();
+        int faveNum = GetFavNum();
+        birthYear = GetBirthYear(out birthYear);
+        int squaredNum = SquareNum(faveNum);
+        DisplayResult(name, squaredNum, birthYear);
     }
 }
